0f7e33f3aab4d2219849fe98b749bccee2e7732c
YoonDong <qpslti1033@gmail.com>
2023-10-29 04:04:18 +0900

    드론 수평유지 적용
    수평유지, 좌우 회전, 전진 후진, 좌우 이동 등 드론 움직임 보정

--- Drone_Start.cs
+++ Drone_Start.cs
@@ -15,6 +15,11 @@ public class PropStart : MonoBehaviour
     private bool EngineData = false;
     private bool isChecking = false;
 
+    public float horizontalReturnSpeed = 1f; // 추가: 수평 상태로 돌아오는 속도
+    public float torqueReturnSpeed = 1f; // 추가: 토크가 0으로 돌아오는 속도
+
+    public float maxTiltAngle = 30f; //드론의 최대 기울기 각도
+
     struct MyData
     {
         public int throttle;
@@ -53,18 +58,81 @@ public class PropStart : MonoBehaviour
         // 시동이 켜진 상태에서만 명령 수행
         if (EngineData)
         {
+
             // 드론 상승
             if (data.throttle > 10)
             {
-                float appliedLiftForce = liftForce * ((data.throttle - 10) / 100f);
-                droneRigidbody.AddForce(transform.up * appliedLiftForce);
+               float appliedLiftForce = liftForce * ((data.throttle - 10) / 100f);
+               droneRigidbody.AddForce(transform.up * appliedLiftForce);
+            }
+            //드론 좌우 회전
+            if (data.yaw > 130) // 오른쪽으로 회전
+            {
+                droneRigidbody.AddTorque(transform.up * (data.yaw - 130) / 10f);
+            } 
+            else if (data.yaw < 125) // 왼쪽으로 회전
+            {
+                droneRigidbody.AddTorque(transform.up * -(125 - data.yaw) / 10f);
+            }
+
+            //드론 수평 유지
+            if ((data.pitch >= 125 && data.pitch <= 130) && (data.roll >= 125 && data.roll <= 130))
+            {
+                Quaternion targetRotation = Quaternion.Euler(0, droneRigidbody.rotation.eulerAngles.y, 0); // 목표 회전(수평 상태, y축 회전 값은 현재 값 유지)
+                droneRigidbody.rotation = Quaternion.Slerp(droneRigidbody.rotation, targetRotation, horizontalReturnSpeed * Time.deltaTime);
+
+                // 토크를 서서히 0으로 만듦
+                Vector3 currentTorque = droneRigidbody.angularVelocity;
+                droneRigidbody.angularVelocity = Vector3.Lerp(currentTorque, Vector3.zero, torqueReturnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (data.pitch > 130) // 드론후진
+                {
+                    droneRigidbody.AddTorque(transform.right * -(data.pitch - 130) / 10f);
+                }
+                else if (data.pitch < 125) // 드론전진
+                {
+                    droneRigidbody.AddTorque(transform.right * (125 - data.pitch) / 10f);
+                }
+
+                // 드론 좌우 이동
+                if (data.roll > 130) // 왼쪽으로 이동
+                {
+                    droneRigidbody.AddTorque(transform.forward * (data.roll - 130) / 10f);
+                } 
+                else if (data.roll < 125) // 오른쪽으로 이동
+                {
+                    droneRigidbody.AddTorque(transform.forward * -(125 - data.roll) / 10f);
+                }
             }
 
             for (int i = 0; i < propellers.Length; i++)
             {
                 propellers[i].transform.Rotate(rotationAxes[i], currentRotationSpeed * Time.deltaTime);
             }
+
+            LimitTiltAngle();   // 드론이 움직이는 최대 기울기 각도 제한 함수
         }
+    }
+
+    private void LimitTiltAngle() // 드론이 움직이는 최대 기울기 각도 제한 함수
+    {
+        Vector3 currentRotation = droneRigidbody.rotation.eulerAngles;
+
+        if (currentRotation.x > 180f) currentRotation.x -= 360f;
+        if (Mathf.Abs(currentRotation.x) > maxTiltAngle)
+        {
+            currentRotation.x = maxTiltAngle * Mathf.Sign(currentRotation.x);
+        }
+
+        if (currentRotation.z > 180f) currentRotation.z -= 360f;
+        if (Mathf.Abs(currentRotation.z) > maxTiltAngle)
+        {
+            currentRotation.z = maxTiltAngle * Mathf.Sign(currentRotation.z);
+        }
+
+        droneRigidbody.rotation = Quaternion.Euler(currentRotation);
     }
 
     private IEnumerator CheckEngineStart()
